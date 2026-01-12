518d0e6c57513f914061c5b17bab21fb05d2fdb2
Hugo CREPIN <crepin.hugo30@gmail.com>
2026-01-12 16:27:59 +0100

    fix swagger

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-import * as YAML from 'yaml';
-import swaggerUi from 'swagger-ui-express';
-import addressController from './adapters/driving/addressController';
-import path from 'path';
+import express from "express";
+import * as YAML from "yaml";
+import swaggerUi from "swagger-ui-express";
+import addressController from "./adapters/driving/addressController";
+import path from "path";
 import * as fs from "node:fs";
 
 const app = express();
 app.use(express.json());
 
-const file  = fs.readFileSync('./openapi.yaml', 'utf8')
-const swaggerDocument = YAML.parse(file)
+const file = fs.readFileSync("./openapi.yaml", "utf8");
+const swaggerDocument = YAML.parse(file);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use('/addresses', addressController);
+app.use("/addresses", addressController);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
